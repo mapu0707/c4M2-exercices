379f8ccd00c1a9a0f14bdbe97d8b1f837a9a4761
Maria Paula Rojas <mariaprojas951@gmail.com>
2025-08-04 17:44:17 -0500

    primera clase

--- src/CuentaBancaria.java
+++ src/CuentaBancaria.java
@@ -1,3 +1,50 @@
 public class CuentaBancaria {
-    
+    // Atributes
+    private String numeroCuenta;
+    private String nombreTitular;
+    private Double saldo;
+    private Double monto;
+
+    // Constructors
+    public CuentaBancaria(String numeroCuenta, String nombreTitular) {
+        this.numeroCuenta = numeroCuenta;
+        this.nombreTitular = nombreTitular;
+    }
+
+    public CuentaBancaria(String numeroCuenta, String nombreTitular, Double saldo) {
+        this.numeroCuenta = numeroCuenta;
+        this.nombreTitular = nombreTitular;
+        this.saldo = saldo;
+    }
+
+    // Getters
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public String getNombreTitular() {
+        return nombreTitular;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(Double monto) {
+        if (monto >= 0) {
+            saldo += monto;
+        } else {
+            System.out.println("El numero debe ser positivo");
+        }
+
+    }
+
+    public void retirar(Double monto) {
+
+        if (monto >= 0 && monto<saldo) {
+            saldo -= monto;
+        } else {
+            System.out.println("Debe tener saldo suficiente");
+        }
+    }
 }
